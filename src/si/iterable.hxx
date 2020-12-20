13b345e1c94657b1a9c335bdf88ea55dcc46e332
Felix Bytow <drako@drako.guru>
2020-12-20 21:02:20 +0100

    :sparkles: added wrapper for standard C++ iterators

--- src/si/iterable.hxx
+++ src/si/iterable.hxx
@@ -3,13 +3,77 @@
 #ifndef SI_ITERABLE_HXX
 #define SI_ITERABLE_HXX
 
+#include "exceptions.hxx"
 #include "iterator.hxx"
 
+#include <iterator>
+#include <type_traits>
+
 namespace si {
-  template <typename T, typename Value>
+  template<typename T, typename Value>
   concept Iterable = requires(T iterable) {
     { iterable.iterator() } -> si::Iterator<Value>;
   };
+
+  template<typename T>
+  concept LegacyIterable = requires(T const& iterable) {
+    { std::cbegin(iterable) };
+    { std::cend(iterable) };
+  };
+
+  namespace detail {
+    template<typename LegacyIterator>
+    class LegacyIterableWrappingIterator final {
+      using ValueType = typename std::iterator_traits<LegacyIterator>::value_type;
+
+      LegacyIterator begin;
+      LegacyIterator end;
+
+    public:
+      inline LegacyIterableWrappingIterator(LegacyIterator begin, LegacyIterator end)
+          :begin{begin}, end{end} { }
+
+      [[nodiscard]] inline bool has_next() const
+      {
+        return begin!=end;
+      }
+
+      inline ValueType next()
+      {
+        if (!has_next())
+          throw si::no_such_element_exception{"Called next() on legacy end iterator."};
+
+        return *begin++;
+      }
+    };
+
+    template<typename LegacyIterator>
+    class LegacyIterableWrapper final {
+      LegacyIterator begin;
+      LegacyIterator end;
+
+    public:
+      inline LegacyIterableWrapper(LegacyIterator begin, LegacyIterator end)
+          :begin{begin}, end{end} { }
+
+      inline auto iterator() const
+      {
+        return LegacyIterableWrappingIterator{begin, end};
+      }
+    };
+  }
+
+  template<typename Iterator>
+  inline auto iterate(Iterator const& begin, Iterator const& end)
+  {
+    return detail::LegacyIterableWrapper{begin, end};
+  }
+
+  template<LegacyIterable LI>
+  inline auto iterate(LI const& li)
+  {
+    return iterate(std::cbegin(li), std::cend(li));
+  }
 }
 
 #endif // SI_ITERABLE_HXX
--- test/iterable_tests.cxx
+++ test/iterable_tests.cxx
@@ -1,4 +1,10 @@
-#include <si/iterable.hxx>
+#include <gtest/gtest.h>
+
+#include <si/si.hxx>
+
+#include <array>
+#include <list>
+#include <vector>
 
 // test structures to see whether the concept is working as intended
 struct IntIterator {
@@ -18,3 +24,71 @@ struct MissingIterator {
 static_assert(si::Iterable<IntIterable, int>, "IntIterable should be an si::Iterable<int>");
 static_assert(!si::Iterable<IntIterable, float>, "IntIterable should not be an si::Iterable<float>");
 static_assert(!si::Iterable<MissingIterator, int>, "MissingIterator should not be an si::Iterable<?>");
+
+TEST(IterableTests, LegacyWrapperVector)
+{
+  std::vector const expected{13, 42, 1337};
+  std::vector<int> results;
+  results.reserve(3u);
+
+  si::iterate(expected)
+      << si::for_each<int>([&results](int n) { results.push_back(n); });
+
+  EXPECT_EQ(results, expected);
+}
+
+TEST(IterableTests, LegacyWrapperList)
+{
+  std::list const expected{13, 42, 1337};
+  std::list<int> results;
+
+  si::iterate(expected)
+      << si::for_each<int>([&results](int n) { results.push_back(n); });
+
+  EXPECT_EQ(results, expected);
+}
+
+TEST(IterableTests, LegacyWrapperArray)
+{
+  std::array const expected{13, 42, 1337};
+  std::array<int, 3u> results{};
+
+  si::iterate(expected)
+      << si::indexed<int>()
+      << si::for_each<std::pair<std::size_t, int>>([&results](std::pair<std::size_t, int> p) {
+        auto const[idx, n] = p;
+        results[idx] = n;
+      });
+
+  EXPECT_EQ(results, expected);
+}
+
+TEST(IterableTests, LegacyWrapperRawArray)
+{
+  int const source[] = {13, 42, 1337};
+  std::array const expected{13, 42, 1337};
+  std::array<int, 3u> results{};
+
+  si::iterate(source)
+      << si::indexed<int>()
+      << si::for_each<std::pair<std::size_t, int>>([&results](std::pair<std::size_t, int> p) {
+        auto const[idx, n] = p;
+        results[idx] = n;
+      });
+
+  EXPECT_EQ(results, expected);
+}
+
+TEST(IterableTests, LegacyWrapperPointers)
+{
+  char const* argv[] = {"alpha", "bravo", "charlie"};
+  std::list const expected{"alpha", "bravo", "charlie"};
+  std::list<char const*> results;
+
+  si::iterate(argv+0, argv+3)
+      << si::for_each<char const*>([&results](char const* arg) {
+        results.push_back(arg);
+      });
+
+  EXPECT_EQ(results, expected);
+}
