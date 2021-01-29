a7aa5cbe5524b3f49443344b12f2412ad9d380c9
Felix Bytow <drako@drako.guru>
2021-01-29 07:41:20 +0100

    :sparkles: trying code coverage

---
+++ codecov.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+BUILD_DIR=build
+
+function codecov {
+  local folder=$1
+  shift
+  local target=$1
+  shift
+  local files=("$@")
+  for filename in "${files[@]}";
+  do
+    local source="$folder/$filename"
+    local notes="$BUILD_DIR/$folder/CMakeFiles/$target.dir/$filename.gcno"
+    #echo "$source -> $notes"
+    gcov-10 -n -o "$notes" "$source" > /dev/null
+  done
+}
+
+cd src || exit
+sources=(**/*.cxx)
+cd ..
+codecov src si "${sources[@]}"
+
+cd test || exit
+tests=(*.cxx)
+cd ..
+codecov test si_tests "${tests[@]}"
+
+#for filename in ../**/*.[ch]xx;
+#do
+#  echo "$filename"
+#  gcov-10 -n -o . "$filename" > /dev/null
+#done
